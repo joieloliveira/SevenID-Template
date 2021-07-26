c880edcd03f923e6ce592b0ed8f992de8a7fabb3
Joiel oliveira <joieloliveira@gmail.com>
2021-07-26 19:45:48 -0300

    PG compliance finalizada sem mobile

--- js/sevenID.js
+++ js/sevenID.js
@@ -1,8 +1,8 @@
-function escolas(opc){
+function escolas(opc) {
 
     let abaActive = opc;
-    
-    if (abaActive=="High-Schools"){
+
+    if (abaActive == "High-Schools") {
         $('#btnHigh-Schools').css("background-color", "rgba(194,255,71)");
         $('#P-High-Schools').css("opacity", "1");
 
@@ -11,9 +11,9 @@ function escolas(opc){
 
         $('.escolasBodyBox3').css("display", "flex");
         $('.escolasBodyBox2').css("display", "none");
-        
+
     }
-    if (abaActive=="Universidades"){
+    if (abaActive == "Universidades") {
         $('#btnUniversidades').css("background-color", "rgba(194,255,71)");
         $('#P-Universidades').css("opacity", "1");
 
@@ -22,5 +22,25 @@ function escolas(opc){
 
         $('.escolasBodyBox2').css("display", "flex");
         $('.escolasBodyBox3').css("display", "none");
+    }
+}
+function Compliance(opc) {
+
+    event.preventDefault()
+    
+    let buton = opc;
+
+    if (buton == "EDITAR") {
+        $('.btnComplianceEDITAR').css("display", "none");
+        $('.btnComplianceSALVAR').css("display", "flex");
+        $('.complianceTdShadow').css("display", "none");
+        $('.complianceTdShadowInp').css("display", "flex");
+
+    }
+    if (buton == "SALVAR") {
+        $('.btnComplianceSALVAR').css("display", "none");
+        $('.btnComplianceEDITAR').css("display", "flex");
+        $('.complianceTdShadow').css("display", "flex");
+        $('.complianceTdShadowInp').css("display", "none");
     }
 }
